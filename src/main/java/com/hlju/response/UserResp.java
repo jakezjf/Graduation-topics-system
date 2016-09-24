2d5001f8e63a261642cd3bef5a0282874d096947
jakezjf <873027371@qq.com>
2016-09-24 10:55:01 +0800

    create UserResp.java

---
+++ src/main/java/com/hlju/response/UserResp.java
@@ -0,0 +1,47 @@
+package com.hlju.response;
+
+/**
+ * Created by JF on 2016/9/22.
+ */
+public class UserResp {
+
+    private String userId;
+
+    private Integer userType;
+
+    private String userPhone;
+
+    private String userEmail;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
