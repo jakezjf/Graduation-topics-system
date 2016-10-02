f93a4e119476124977ad59a680575fe614114a75
jakezjf <873027371@qq.com>
2016-10-02 16:24:55 +0800

    增加 studentMapper

---
+++ src/main/java/com/hlju/dao/StudentMapper.java
@@ -0,0 +1,23 @@
+package com.hlju.dao;
+
+import com.hlju.model.Student;
+
+import java.util.List;
+
+import com.hlju.response.StudentResp;
+
+public interface StudentMapper {
+
+    int deleteByPrimaryKey(String stuId);
+
+    Student selectByPrimaryKey(String stuId);
+
+    int updateByPrimaryKeySelective(Student student);
+
+    List<StudentResp> getStudentRespList(Student student);
+
+    int insertSelective(Student student);
+
+    StudentResp getStudentResp(Student student);
+
+}
