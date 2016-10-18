b22f0418a7e49919ea3d125f1b8725d2bb2b080d
jakezjf <873027371@qq.com>
2016-10-18 21:28:28 +0800

    增加 Major model

---
+++ src/main/java/com/hlju/model/Major.java
@@ -0,0 +1,74 @@
+package com.hlju.model;
+
+public class Major {
+
+    private Integer majorId; //专业ID
+
+    private String majorName; //专业名字
+
+    private Integer collegeId; //学院ID
+
+    private String back1;
+
+    private String back2;
+
+    private String back3;
+
+    private String search;
+
+    public Integer getMajorId() {
+        return majorId;
+    }
+
+    public void setMajorId(Integer majorId) {
+        this.majorId = majorId;
+    }
+
+    public String getMajorName() {
+        return majorName;
+    }
+
+    public void setMajorName(String majorName) {
+        this.majorName = majorName == null ? null : majorName.trim();
+    }
+
+    public Integer getCollegeId() {
+        return collegeId;
+    }
+
+    public void setCollegeId(Integer collegeId) {
+        this.collegeId = collegeId;
+    }
+
+    public String getBack1() {
+        return back1;
+    }
+
+    public void setBack1(String back1) {
+        this.back1 = back1 == null ? null : back1.trim();
+    }
+
+    public String getBack2() {
+        return back2;
+    }
+
+    public void setBack2(String back2) {
+        this.back2 = back2 == null ? null : back2.trim();
+    }
+
+    public String getBack3() {
+        return back3;
+    }
+
+    public void setBack3(String back3) {
+        this.back3 = back3 == null ? null : back3.trim();
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
