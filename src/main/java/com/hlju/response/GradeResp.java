9cc7e2a811bb03afbacc3c9e6a8092e46d68c256
jakezjf <873027371@qq.com>
2016-11-18 20:42:34 +0800

    create Grade and GradeResp

---
+++ src/main/java/com/hlju/response/GradeResp.java
@@ -0,0 +1,27 @@
+package com.hlju.response;
+
+/**
+ * Created by JF on 2016/9/28.
+ */
+public class GradeResp {
+
+    private Integer gradeId; //年级ID
+
+    private String gradeName; //年级名字
+
+    public Integer getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(Integer gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public String getGradeName() {
+        return gradeName;
+    }
+
+    public void setGradeName(String gradeName) {
+        this.gradeName = gradeName;
+    }
+}
